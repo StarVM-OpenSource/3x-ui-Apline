3fd762dbeac44be11eaa2a77ca181acd6e4f97ad
欢 <2711452309@qq.com>
2025-07-30 14:13:44 +0800

    Create glibc.sh

---
+++ glibc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 添加密钥
+curl -Lo /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
+
+# 下载 glibc 安装包（注意：一定要用具体版本链接）
+curl -Lo glibc.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.34-r0/glibc-2.34-r0.apk
+
+# 安装
+apk add glibc.apk
