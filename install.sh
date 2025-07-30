06a3bfb194177f933e0c61cdfe152d2805b4c2cf
欢 <2711452309@qq.com>
2025-07-30 21:27:14 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash <(curl -Ls https://raw.githubusercontent.com/StarVM-OpenSource/3x-ui-Apline/refs/heads/main/glibc.sh)
+
+bash <(curl -Ls https://raw.githubusercontent.com/StarVM-OpenSource/3x-ui-Apline/refs/heads/main/install_alpine.sh)
